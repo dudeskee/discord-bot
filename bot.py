4a63f20d6b1f05ff8c0ebcd3e8c7985b7ed44d8e
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2020-12-14 14:05:51 -0600

    add automatic reactions

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
+    # COMMAND: !addVote
     if message.content == "!addVote":
         # get history
         messages = await message.channel.history(limit=3).flatten()
@@ -36,6 +37,12 @@ async def on_message(message):
 
         # delete command
         await message.delete()
+
+    # PASSIVE: auto-react meme-off/hot-takes
+    if (message.channel.name == "meme-off") or (message.channel.name == "hot-takes"):
+        # add reactions
+        await message.add_reaction("updoot:692862052599070720")
+        await message.add_reaction("downdoot:692862024241250334")
 
 
 client.run(TOKEN)
