eec73ff53fe83f8c4e36305a2ff7302ac2095323
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2020-12-14 13:50:25 -0600

    add addVote command

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     for guild in client.guilds:
@@ -20,5 +21,21 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+
+@client.event
+async def on_message(message):
+
+    if message.content == "!addVote":
+        # get history
+        messages = await message.channel.history(limit=3).flatten()
+
+        # add reactions
+        await messages[1].add_reaction("updoot:692862052599070720")
+        await messages[1].add_reaction("downdoot:692862024241250334")
+
+        # delete command
+        await message.delete()
+
 
 client.run(TOKEN)
