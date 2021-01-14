8a37ed9f49d9bd180ad1dfa267f2173d6adc2808
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2021-01-14 18:02:18 +0000

    dooskee

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
-# bot.py
-import os
-
 import discord
+import os
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
 
 
 @client.event
@@ -21,106 +20,202 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+    await client.change_presence(activity=discord.Game('gumpin\' bubbies'))
+
 
 
 @client.event
-async def on_message(message):
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
 
-    # COMMAND: !addVote
-    if message.content == "!addVote":
-        # get history
-        messages = await message.channel.history(limit=3).flatten()
 
-        # add reactions
-        await messages[1].add_reaction("updoot:692862052599070720")
-        await messages[1].add_reaction("downdoot:692862024241250334")
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
 
-        # delete command
-        await message.delete()
 
-    # COMMAND: !addBigVote##
-    if "!addBigVote" in message.content:
-        # get history
-        messages = await message.channel.history(limit=3).flatten()
+# load cogs
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
-        # add reactions
-        if message.content[-1] == "1":
-            await messages[1].add_reaction("1️⃣")
-        elif message.content[-1] == "2":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-        elif message.content[-1] == "3":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-        elif message.content[-1] == "4":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-            await messages[1].add_reaction("4️⃣")
-        elif message.content[-1] == "5":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-            await messages[1].add_reaction("4️⃣")
-            await messages[1].add_reaction("5️⃣")
-        elif message.content[-1] == "6":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-            await messages[1].add_reaction("4️⃣")
-            await messages[1].add_reaction("5️⃣")
-            await messages[1].add_reaction("6️⃣")
-        elif message.content[-1] == "7":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-            await messages[1].add_reaction("4️⃣")
-            await messages[1].add_reaction("5️⃣")
-            await messages[1].add_reaction("6️⃣")
-            await messages[1].add_reaction("7️⃣")
-        elif message.content[-1] == "8":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-            await messages[1].add_reaction("4️⃣")
-            await messages[1].add_reaction("5️⃣")
-            await messages[1].add_reaction("6️⃣")
-            await messages[1].add_reaction("7️⃣")
-            await messages[1].add_reaction("8️⃣")
-        elif message.content[-1] == "9":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-            await messages[1].add_reaction("4️⃣")
-            await messages[1].add_reaction("5️⃣")
-            await messages[1].add_reaction("6️⃣")
-            await messages[1].add_reaction("7️⃣")
-            await messages[1].add_reaction("8️⃣")
-            await messages[1].add_reaction("9️⃣")
-        elif message.content[-1] == "0":
-            await messages[1].add_reaction("1️⃣")
-            await messages[1].add_reaction("2️⃣")
-            await messages[1].add_reaction("3️⃣")
-            await messages[1].add_reaction("4️⃣")
-            await messages[1].add_reaction("5️⃣")
-            await messages[1].add_reaction("6️⃣")
-            await messages[1].add_reaction("7️⃣")
-            await messages[1].add_reaction("8️⃣")
-            await messages[1].add_reaction("9️⃣")
-            await messages[1].add_reaction("0️⃣")
-        else:
-            await message.channel.send("Invalid input")
 
-        # delete command
-        await message.delete()
+# unload cogs
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
 
+
+# For future use with cogs - remove # when cogs configured
+#for filename in os.listdir('./cogs'):
+#    if filename.endswith('.py'):
+#        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+# ping command - reports bot latency
+@client.command()
+@commands.cooldown(1, 30, commands.BucketType.guild)
+async def ping(ctx):
+    if commands.errors.CommandOnCooldown:
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    else:
+        await ctx.send('nope')
+
+
+# kick command - must have punisher role
+@client.command(aliases=['boot', 'punt'])
+@commands.has_role("punisher")
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.channel.purge(limit=1)
+    await ctx.send(f'{member} was kicked for {reason}!')
+
+
+# ban command - must have punisher role
+@client.command()
+@commands.has_role("punisher")
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    # delete command
+    await ctx.channel.purge(limit=1)
+    await ctx.send(f'{member} was banned for {reason}!')
+
+
+# unban command - must have punisher role
+@client.command()
+@commands.has_role("punisher")
+async def unban(ctx, *, member):
+    await ctx.channel.purge(limit=1)
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
+@client.command(aliases=['addvote', 'vote', 'doots', 'doot'])
+async def add_vote(ctx, amount=1):
+    # get history
+    messages = await ctx.channel.history(limit=3).flatten()
+    # delete command
+    await ctx.channel.purge(limit=amount)
+    # add reactions
+    await messages[1].add_reaction("updoot:692862052599070720")  # 692862052599070720
+    await messages[1].add_reaction("downdoot:692862024241250334")  # 692862024241250334
+
+
+@client.command()
+async def clear(ctx, amount):
+    await ctx.channel.purge(limit=amount)
+
+
+@client.command(aliases=['bigvote', 'numvote', 'nv'])
+async def add_big_vote(ctx, amount: int, prge=1):
+    msg = await ctx.channel.history(limit=3).flatten()
+    if amount == 1:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+
+    elif amount == 2:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+
+    elif amount == 3:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+
+    elif amount == 4:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+        await msg[1].add_reaction("4️⃣")
+
+    elif amount == 5:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+        await msg[1].add_reaction("4️⃣")
+        await msg[1].add_reaction("5️⃣")
+
+    elif amount == 6:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+        await msg[1].add_reaction("4️⃣")
+        await msg[1].add_reaction("5️⃣")
+        await msg[1].add_reaction("6️⃣")
+
+    elif amount == 7:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+        await msg[1].add_reaction("4️⃣")
+        await msg[1].add_reaction("5️⃣")
+        await msg[1].add_reaction("6️⃣")
+        await msg[1].add_reaction("7️⃣")
+
+    elif amount == 8:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+        await msg[1].add_reaction("4️⃣")
+        await msg[1].add_reaction("5️⃣")
+        await msg[1].add_reaction("6️⃣")
+        await msg[1].add_reaction("7️⃣")
+        await msg[1].add_reaction("8️⃣")
+
+    elif amount == 9:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+        await msg[1].add_reaction("4️⃣")
+        await msg[1].add_reaction("5️⃣")
+        await msg[1].add_reaction("6️⃣")
+        await msg[1].add_reaction("7️⃣")
+        await msg[1].add_reaction("8️⃣")
+        await msg[1].add_reaction("9️⃣")
+
+    elif amount == 0:
+        await ctx.channel.purge(limit=prge)
+        await msg[1].add_reaction("1️⃣")
+        await msg[1].add_reaction("2️⃣")
+        await msg[1].add_reaction("3️⃣")
+        await msg[1].add_reaction("4️⃣")
+        await msg[1].add_reaction("5️⃣")
+        await msg[1].add_reaction("6️⃣")
+        await msg[1].add_reaction("7️⃣")
+        await msg[1].add_reaction("8️⃣")
+        await msg[1].add_reaction("9️⃣")
+        await msg[1].add_reaction("0️⃣")
+
+    else:
+        await ctx.send(f'{amount} is an invalid input. Single numbers, 0 to 9.')
+
+
+
+@client.event
+async def passive_votes(ctx):
     # PASSIVE: auto-react meme-off/hot-takes
-    if (message.channel.name == "meme-off") or (message.channel.name == "hot-takes") \
-            and (message.author.id != 785635869771563018):
+    if (ctx.channel.name == "meme-off") or (ctx.channel.name == "hot-takes") \
+            and (ctx.author.id != 785635869771563018):
         # add reactions
-        await message.add_reaction("updoot:692862052599070720")
-        await message.add_reaction("downdoot:692862024241250334")
+        await ctx.add_reaction("updoot:692862052599070720")
+        await ctx.add_reaction("downdoot:692862024241250334")
 
 
 client.run(TOKEN)
